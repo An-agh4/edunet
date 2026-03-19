affa1d225c81d4ee67ce9ff2ddce73f1e0c01024
Anagha Ramesh K <anagharamesh54@gmail.com>
2026-03-19 14:03:41 +0530

    Change API key to placeholder in gemini_client.py
    Updated API key placeholder in gemini_client.py

--- Chatbot/chatbot/chat/gemini_client.py
+++ Chatbot/chatbot/chat/gemini_client.py
@@ -2,7 +2,7 @@
 from google import genai
 
 # The client gets the API key from the environment variable `GEMINI_API_KEY`.
-client = genai.Client(api_key="AIzaSyBzgUdwmaA9wfhQfS4DipQI0nr0CubKMbc")
+client = genai.Client(api_key="Keep you API key here")
 
 def get_response(prompt):
     response = client.models.generate_content(
